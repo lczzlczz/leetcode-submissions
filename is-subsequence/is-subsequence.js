a7ef961c341b9239bb98a13383c940213738220b
lcssptz <lcssptz@gmail.com>
2023-06-20 18:48:53 +0800

    Time: 61 ms (53.78%) | Memory: 42.2 MB (35.69%) - LeetSync

--- is-subsequence/is-subsequence.js
+++ is-subsequence/is-subsequence.js
@@ -4,15 +4,9 @@
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-  let [p, q] = [0, 0];
-  while (p < s.length) {
-    if (q === t.length) return false;
-    if (s[p] === t[q]) {
-      p++;
-      q++;
-    } else {
-      q++;
-    }
+  let p = 0;
+  for (let i = 0; i < t.length; i++) {
+    if (s[p] === t[i]) p++;
   }
-  return true;
+  return p === s.length; 
 };
