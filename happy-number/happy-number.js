94388051d51d824755d33aa98e80b282d6da0775
lcssptz <lcssptz@gmail.com>
2023-06-23 03:18:39 +0800

    Time: 62 ms (74.25%) | Memory: 41.9 MB (98.26%) - LeetSync

---
+++ happy-number/happy-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  while (n > 9) {
+    let sum = 0;
+    while (n > 0) {
+      sum += Math.pow(n % 10, 2);
+      n = Math.floor(n / 10);
+    }
+    if (sum === 1) return true;
+    n = sum;
+  }
+  return n === 1 || n === 7? true: false;
+};
