eb993d260e027156e78b72e54581caac7da348ea
lcssptz <lcssptz@gmail.com>
2023-06-28 17:04:03 +0800

    Time: 63 ms (38.02%) | Memory: 42.4 MB (36.31%) - LeetSync

---
+++ rotate-image/rotate-image.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+  const n = matrix.length;
+
+  matrix.reverse();
+
+  swap(matrix, n);
+};
+
+const swap = (matrix, n) => {
+  for (let i = 0; i < n; i++) {
+    for (let j = i; j < n; j++) {
+      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]]
+    }
+  }
+}
