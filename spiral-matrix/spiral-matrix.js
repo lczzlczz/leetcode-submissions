d239e383de4429c19ff8a7506f12129372a88d92
lcssptz <lcssptz@gmail.com>
2023-06-28 14:51:22 +0800

    Time: 60 ms (41.83%) | Memory: 41.7 MB (60.27%) - LeetSync

--- spiral-matrix/spiral-matrix.js
+++ spiral-matrix/spiral-matrix.js
@@ -2,34 +2,49 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-var spiralOrder = function(matrix) {
-  const m = matrix.length;
-  const n = matrix?.[0].length;
-  //       N  E  S  W 
-  //       0  1  2  3 
-  let d = [0, 1, 0, 0];
-  let res = [];
 
+// Simulate the process
+var spiralOrder = function(matrix) {
+  // Create an m x n array to store visit state
+  const [m, n] = [matrix.length, matrix?.[0].length];
   let visited = new Array(m).fill().map(() => new Array(n).fill(0));
-  console.log(visited);
+
+  // Direction:   N  E  S  W
+  // To North => [1, 0, 0, 0]
+  // To East  => [0, 1, 0, 0]
+  // To South => [0, 0, 1, 0]
+  // To West  => [0, 0, 0, 1]
+  // Direction:   N  E  S  W
+  let dr =       [0, 1, 0, 0]; // Start by moving to the East
+
+  let res = [];
   let [i, j] = [0, 0];
   while (visited[i]?.[j] === 0) {
     res.push(matrix[i][j]);
     visited[i][j] = 1;
 
-    if (shouldTurn(i, j, visited, d)) {
-      d.unshift(d.pop());
+    if (isCollided(i, j, visited, dr)) {
+      turnRight(dr);
     }
 
-    i = i - d[0] + d[2];
-    j = j + d[1] - d[3];
-  }
+    [i, j] = moveNext(i, j, dr);
+ }
 
-  return res;
+ return res;
 };
 
-const shouldTurn = (i, j, visited, d) => {
-  const nextI = i - d[0] + d[2];
-  const nextJ = j + d[1] - d[3];
-  return visited[nextI]?.[nextJ] === 1 || visited[nextI]?.[nextJ] === undefined;
+// Check if the next(i, j) cooridinate is out of boundary or visited
+const isCollided = (i, j, visited, dr) => {
+  const [nextI, nextJ] = moveNext(i, j, dr);
+  return visited[nextI]?.[nextJ] !== 0;
+}
+
+// Turn the direction to the right
+const turnRight = dr => {
+  dr.unshift(dr.pop());
+}
+
+// Calculate the next(i, j) coordinate based on the current direction
+const moveNext = (i, j, dr) => {
+  return [i - dr[0] + dr[2], j + dr[1] - dr[3]];
 }
