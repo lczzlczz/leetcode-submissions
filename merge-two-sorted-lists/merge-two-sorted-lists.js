3d9b24a3a82c606539f9203d09d0cab80a424f49
lcssptz <lcssptz@gmail.com>
2023-06-30 16:51:06 +0800

    Time: 65 ms (71.89%) | Memory: 44.1 MB (61.62%) - LeetSync

---
+++ merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  const dummy = new ListNode;
+  let head = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      head.next = list1;
+      list1 = list1.next;
+    } else {
+      head.next = list2;
+      list2 = list2.next;
+    }
+    head = head.next;
+  }
+
+  head.next = list1 !== null? list1: list2;
+
+  return dummy.next;
+};
