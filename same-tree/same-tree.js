f3bba326e9840a85e21ef98688c97fd39b1bfac8
lcssptz <lcssptz@gmail.com>
2023-07-09 10:22:21 +0800

    Time: 56 ms (71.19%) | Memory: 42.9 MB (5.00%) - LeetSync

--- same-tree/same-tree.js
+++ same-tree/same-tree.js
@@ -12,12 +12,8 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    const find = (p, q) => {
-      if (!p && !q) return true;
-      if (!p || !q) return false;
-      if (p.val !== q.val) return false;
-      return find(p.left, q.left) && find(p.right, q.right);
-    }
-
-  return find(p, q);
+  if (!p && !q) return true;
+  if (!p || !q) return false;
+  if (p.val !== q.val) return false;
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
