d4faa99875c1e69a8327ec62112dc8d00c749933
lcssptz <lcssptz@gmail.com>
2023-07-09 10:12:02 +0800

    Time: 60 ms (50.44%) | Memory: 42.5 MB (20.24%) - LeetSync

---
+++ same-tree/same-tree.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    const find = (p, q) => {
+      if (!p && !q) return true;
+      if (!p && q) return false;
+      if (p && !q) return false;
+      if (p && q && (p.val !== q.val)) return false;
+      return find(p.left, q.left) && find(p.right, q.right);
+    }
+
+  return find(p, q);
+};
