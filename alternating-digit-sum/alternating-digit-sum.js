3a3da330bab6d15da624ec865a570bc4b9ce1b69
lcssptz <lcssptz@gmail.com>
2023-07-12 01:13:55 +0800

    Time: 51 ms (91.40%) | Memory: 41.7 MB (86.88%) - LeetSync

--- alternating-digit-sum/alternating-digit-sum.js
+++ alternating-digit-sum/alternating-digit-sum.js
@@ -4,15 +4,13 @@
  */
 var alternateDigitSum = function(n) {
   let res = 0;
-  let sign = 1;
+  let sign = -1;
 
   while (n > 0) {
-    res += sign * n % 10;
     sign = -sign;
+    res += sign * n % 10;
     n = Math.trunc(n / 10);
   }
-
-  sign = -sign; 
 
   return sign * res;
 };
