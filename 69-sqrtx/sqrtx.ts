2d65fc8dc4eaad4fe2f80b3bc5f882ac969d5fde
lczz <lcssptz@gmail.com>
2023-09-14 11:42:05 +0800

    Time: 78 ms (43.75%) | Memory: 45 MB (27.96%) - LeetSync

--- 69-sqrtx/sqrtx.ts
+++ 69-sqrtx/sqrtx.ts
@@ -1,15 +1,17 @@
 function mySqrt(x: number): number {
   let [left, right] = [0, x];
+  let ans = 0;
 
-  while (left + 1 < right) {
+  while (left <= right) {
     const mid = Math.floor((left + right) / 2);
 
-    if (mid * mid >= x) {
-      right = mid;
+    if (mid * mid <= x) {
+      left = mid + 1;
+      ans = Math.max(ans, mid);
     } else {
-      left = mid;
+      right = mid - 1;
     }
   }
 
-  return right * right <= x ? right: left;
+  return ans;
 };
